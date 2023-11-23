21b80f6e6b1df4963391b66a239b8aad8de2d9e1
ivan <ade.ivanfahmi@gmail.com>
2023-11-23 18:45:21 +0700

    menambahkan component

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -9,9 +9,9 @@
     </div>
 
     @if (session()->has('success'))
-        <div class="alert alert-success mt-4">
-            {{ session()->get('success') }}
-        </div>
+    <x-alert type="success" class="mt-4">
+        {{ session()->get('success') }}
+    </x-alert>
     @endif
 
     <div class="container mt-5">
